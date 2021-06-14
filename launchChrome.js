4daa71ead2b969e5d7c9c083743ab529b3904359
tamnguyen <tamnguyen02121993@gmail.com>
2021-06-14 15:53:36 +0700

    Update source code

--- launchChrome.js
+++ launchChrome.js
@@ -1,96 +1,212 @@
-const { Builder, By, Capabilities } = require("selenium-webdriver");
+const {Builder, By, Capabilities} = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 const fsPromise = require("fs/promises");
 const fs = require("fs");
 const path = require("path");
 
 module.exports = {
-  driver: null,
-  mainWindow: null,
-  async process(comicLink, comicName, selectorImage, selectorListChapter) {
-    this.driver = new Builder()
-      .forBrowser("chrome")
-      .setChromeOptions(new chrome.Options().headless())
-      .build();
-    await this.driver.get(comicLink);
-    this.mainWindow = await this.driver.getWindowHandle();
-
-    let selectElement = await this.driver.findElement(
-      By.css(selectorListChapter)
-    );
-
-    let listChapter = await selectElement.findElements(By.css("option"));
-
-    if (listChapter.length === 0) return false;
-
-    const comicDomain = new URL(comicLink).origin;
-
-    for (const i in listChapter) {
-      let chapterLink = await listChapter[i].getAttribute("value");
-      if (chapterLink.startsWith("/")) {
-        chapterLink = `${comicDomain}${chapterLink}`;
-      }
-      const chapter = await listChapter[i].getText();
-      await this.driver.executeScript(
-        `window.open('${chapterLink}', '_blank');`
-      );
-
-      const chapterWindowHandle = (await this.driver.getAllWindowHandles())[1];
-      await this.driver.switchTo().window(chapterWindowHandle);
-
-      const storedPath = path.join(__dirname, "images", comicName, chapter);
-      const isExistFolder = fs.existsSync(storedPath);
-      if (!isExistFolder) {
-        fs.mkdirSync(storedPath, {
-          recursive: true,
-        });
-      }
-      const imgs = await this.driver.findElements(By.css(selectorImage));
-      if (imgs.length === 0) return false;
-
-      for (const index in imgs) {
-        await this.driver.executeScript(
-          `window.open('${await imgs[index].getAttribute("src")}', '_blank');`
-        );
-      }
-      let windowHandles = await this.driver.getAllWindowHandles();
-      const excuteScript = await fsPromise.readFile(
-        path.join(__dirname, "excuteScript.js"),
-        {
-          encoding: "utf-8",
+    async init(link) {
+        const driver = new Builder().forBrowser("chrome").setChromeOptions(new chrome.Options().headless()).build();
+        await driver.get(link);
+        const mainWindow = await driver.getWindowHandle();
+        return {driver, mainWindow}
+    },
+    createFolder(folderName) {
+        const storedPath = path.join(__dirname, "images", folderName);
+        const isExistFolder = fs.existsSync(storedPath);
+        if (! isExistFolder) {
+            fs.mkdirSync(storedPath, {recursive: true});
         }
-      );
+        return storedPath;
+    },
+    async processDownloadAllChapterComic(comicLink, comicName, imageSelector, listChapterSelector) {
+        const initInstance = await this.init(comicLink);
+        let selectElement = await initInstance.driver.findElement(By.css(listChapterSelector));
 
-      const windowHandlesLength = windowHandles.length;
+        let listChapter = await selectElement.findElements(By.css("option"));
 
-      for (const index in windowHandles) {
-        if (
-          windowHandles[index] === this.mainWindow ||
-          windowHandles[index] === chapterWindowHandle
-        )
-          continue;
-        await this.driver.switchTo().window(windowHandles[index]);
+        if (listChapter.length === 0) 
+            return false;
+        
 
-        let base64 = await this.driver.executeScript(excuteScript);
-        base64 = base64.replace(/^data:image\/\w+;base64,/, "");
-        let buf = Buffer.from(base64, "base64");
-        await fsPromise.writeFile(
-          path.join(storedPath, `${windowHandlesLength - index}.jpeg`),
-          buf
-        );
-      }
-      windowHandles = await this.driver.getAllWindowHandles();
-      for (const index in windowHandles) {
-        if (windowHandles[index] === this.mainWindow) continue;
-        await this.driver.switchTo().window(windowHandles[index]);
-        await this.driver.close();
-      }
+        const comicDomain = new URL(comicLink).origin;
 
-      await this.driver.switchTo().window(this.mainWindow);
+        listChapter = [...new Set(listChapter)];
+
+        for (const i in listChapter) {
+            let chapterLink = await listChapter[i].getAttribute("value");
+            if (chapterLink.startsWith("/")) {
+                chapterLink = `${comicDomain}${chapterLink}`;
+            }
+            const chapter = await listChapter[i].getText();
+            await initInstance.driver.executeScript(`window.open('${chapterLink}', '_blank');`);
+
+            const chapterWindowHandle = (await initInstance.driver.getAllWindowHandles())[1];
+            await initInstance.driver.switchTo().window(chapterWindowHandle);
+
+            const storedPath = path.join(__dirname, "images", comicName, chapter);
+            const isExistFolder = fs.existsSync(storedPath);
+            if (! isExistFolder) {
+                fs.mkdirSync(storedPath, {recursive: true});
+            }
+            const imgElement = await initInstance.driver.findElements(By.css(imageSelector));
+            if (imgElement.length === 0) 
+                return false;
+            
+            let urls = [];
+            for (const index in imgElement) {
+                let url = await imgElement[index].getAttribute("src");
+                if (url.startsWith("/")) {
+                    url = `${comicDomain}${url}`;
+                }
+                urls.push(url);
+            }
+
+            await this.downloadImageByUrl(initInstance.driver, initInstance.mainWindow, urls, storedPath, chapterWindowHandle);
+        }
+
+        await initInstance.driver.switchTo().window(initInstance.mainWindow);
+        await initInstance.driver.close();
+
+    },
+    async processDownloadMultiChapterComic(comicLink, comicName, imageSelector, listChapterSelector, start, end) {
+        const initInstance = await this.init(comicLink);
+        let selectElement = await initInstance.driver.findElement(By.css(listChapterSelector));
+
+        let listChapter = await selectElement.findElements(By.css("option"));
+
+        if (listChapter.length === 0) 
+            return false;
+        
+
+        listChapter = listChapter.slice(start, end);
+
+        const comicDomain = new URL(comicLink).origin;
+        listChapter = [...new Set(listChapter)];
+        for (const i in listChapter) {
+            let chapterLink = await listChapter[i].getAttribute("value");
+            if (chapterLink.startsWith("/")) {
+                chapterLink = `${comicDomain}${chapterLink}`;
+            }
+            const chapter = await listChapter[i].getText();
+            await initInstance.driver.executeScript(`window.open('${chapterLink}', '_blank');`);
+
+            const chapterWindowHandle = (await initInstance.driver.getAllWindowHandles())[1];
+            await initInstance.driver.switchTo().window(chapterWindowHandle);
+
+            const storedPath = path.join(__dirname, "images", comicName, chapter);
+            const isExistFolder = fs.existsSync(storedPath);
+            if (! isExistFolder) {
+                fs.mkdirSync(storedPath, {recursive: true});
+            }
+            const imgElement = await initInstance.driver.findElements(By.css(imageSelector));
+            if (imgElement.length === 0) 
+                return false;
+            
+            let urls = [];
+            for (const index in imgElement) {
+                let url = await imgElement[index].getAttribute("src");
+                if (url.startsWith("/")) {
+                    url = `${comicDomain}${url}`;
+                }
+                urls.push(url);
+            }
+
+            await this.downloadImageByUrl(initInstance.driver, initInstance.mainWindow, urls, storedPath, chapterWindowHandle);
+        }
+
+        await initInstance.driver.switchTo().window(initInstance.mainWindow);
+        await initInstance.driver.close();
+    },
+    async downloadImageByUrl(driver, mainWindow, urls, storedPath, ignoredWindowHandle) {
+        if (urls.length === 0) 
+            return false;
+        
+        for (const index in urls) {
+            await driver.executeScript(`window.open('${
+                urls[index]
+            }', '_blank');`);
+        }
+
+        let windowHandles = await driver.getAllWindowHandles();
+        const excuteScript = await fsPromise.readFile(path.join(__dirname, "excuteScript.js"), {encoding: "utf-8"});
+
+        const windowHandlesLength = windowHandles.length;
+
+        for (const index in windowHandles) {
+            if (windowHandles[index] === mainWindow || windowHandles[index] === ignoredWindowHandle) 
+                continue;
+            
+
+            await driver.switchTo().window(windowHandles[index]);
+
+            let base64 = await driver.executeScript(excuteScript);
+            base64 = base64.replace(/^data:image\/\w+;base64,/, "");
+            let buf = Buffer.from(base64, "base64");
+            await fsPromise.writeFile(path.join(storedPath, `${
+                windowHandlesLength - index
+            }.jpeg`), buf);
+        }
+
+        windowHandles = await driver.getAllWindowHandles();
+        for (const index in windowHandles) {
+            if (windowHandles[index] === mainWindow) 
+                continue;
+            
+
+            await driver.switchTo().window(windowHandles[index]);
+            await driver.close();
+        }
+
+        await driver.switchTo().window(mainWindow);
+    },
+    async processDownloadImageByPattern(link, imageSelector, folderName) {
+        const initInstance = await this.init(link);
+
+        const storedPath = this.createFolder(folderName);
+
+        const imgElements = await initInstance.driver.findElements(By.css(imageSelector));
+        let urls = []
+        const domain = new URL(link).origin;
+        for (const index in imgElements) {
+            let url = await imgElements[index].getAttribute("src");
+            if (url.startsWith("/")) {
+                url = `${domain}${url}`;
+            }
+            urls.push(url);
+        }
+
+        await this.downloadImageByUrl(initInstance.driver, initInstance.mainWindow, urls, storedPath, null);
+        await initInstance.driver.switchTo().window(initInstance.mainWindow);
+        await initInstance.driver.close();
+    },
+    async processDownloadImageByPatternWithMultiPage(link, imageSelector, folderName, pageParamName = 'page', pageStart = 1, pageEnd = 5) {
+        const initInstance = await this.init(link);
+        let urls = [];
+        const domain = new URL(link).origin;
+        for (let index = pageStart; index <= pageEnd; index++) {
+
+            const pageLink = `${link}?${pageParamName}=${index}`;
+            await initInstance.driver.executeScript(`window.open('${pageLink}', '_blank');`);
+            const pageWindowHandle = (await initInstance.driver.getAllWindowHandles())[1];
+            await initInstance.driver.switchTo().window(pageWindowHandle);
+
+            const imgElements = await initInstance.driver.findElements(By.css(imageSelector));
+            for (const index in imgElements) {
+                let url = await imgElements[index].getAttribute("src");
+                if (url.startsWith("/")) {
+                    url = `${domain}${url}`;
+                }
+                urls.push(url);
+            }
+            await initInstance.driver.close();
+            await initInstance.driver.switchTo().window(initInstance.mainWindow);
+        }
+
+        const storedPath = this.createFolder(folderName);
+        await this.downloadImageByUrl(initInstance.driver, initInstance.mainWindow, urls, storedPath, null);
+
+        await initInstance.driver.switchTo().window(initInstance.mainWindow);
+        await initInstance.driver.close();
     }
-
-    await this.driver.switchTo().window(this.mainWindow);
-    await this.driver.close();
-    return true;
-  },
 };
--- src/index.js
+++ src/index.js
@@ -1,70 +1,171 @@
 const express = require("express");
-const exphbs = require("express-handlebars");
-const methodOverride = require("method-override");
 const launchChrome = require("./../launchChrome");
+const rootPath = require('./../getRootPath');
+const archiver = require('archiver');
+const faker = require('faker');
+const fs = require('fs');
+const fsPromise = require('fs/promises');
 const app = express();
 const path = require("path");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const port = 3000;
+const downloadOptions = (randomFolderName) => {
+    return {
+        headers: {
+            'Content-Disposition': `attachment; filename="${randomFolderName}.zip"`
+        }
+    }
+};
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use("/images", express.static(path.join(rootPath, "images")));
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-app.use(methodOverride("_method"));
 app.use(express.urlencoded());
 app.use(express.json());
 
-app.engine(
-  "hbs",
-  exphbs.create({
-    extname: ".hbs",
-    helpers: {
-      raw: function (options) {
-        return options.fn();
-      },
-    },
-  }).engine
-);
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "views"));
+app.post("/download-all-chapter-comic", async (req, res) => {
+    const comicLink = req.body.comicLink;
+    const imageSelector = req.body.imageSelector;
+    const listChapterSelector = req.body.listChapterSelector;
+    const comicName = req.body.comicName;
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+    await launchChrome.processDownloadAllChapterComic(comicLink, comicName, imageSelector, listChapterSelector);
+    const downloadZipFile = path.join(rootPath, 'images', `${comicName}.zip`);
+    const archive = archiver('zip');
+    const output = fs.createWriteStream(downloadZipFile)
 
-app.post("/process", async (req, res) => {
-  const comicLink = req.body.comicLink;
-  const selectorImage = req.body.selectorImage || ".comicDetail2 img";
-  const selectorListChapter =
-    req.body.selectorListChapter || "select.list-chapter:first-child";
-  const comicName = req.body.comicName;
-  if (!comicLink || !comicName) {
-    res.json({
-      error: "Params is invalid!",
-      body: req.body,
+    output.on('close', function () {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+        res.sendFile(downloadZipFile, downloadOptions(comicName))
     });
-  }
 
-  const result = await launchChrome.process(
-    comicLink,
-    comicName,
-    selectorImage,
-    selectorListChapter
-  );
-  if (!result) {
-    res.json({
-      error: "Cannot find element",
+    archive.on('error', function (err) {
+        console.log(err)
+        throw err;
     });
-  }
 
-  res.json({
-    message: "Process successfully!",
-  });
+    archive.pipe(output);
+    archive.directory(path.join(rootPath, 'images', comicName), false);
+    archive.finalize();
 });
 
-app.get("/success", (req, res) => {
-  res.render("success");
+app.post("/download-multi-chapter-comic", async (req, res) => {
+    const comicLink = req.body.comicLink;
+    const imageSelector = req.body.imageSelector;
+    const listChapterSelector = req.body.listChapterSelector;
+    const start = req.body.start;
+    const end = req.body.end;
+    const comicName = `${
+        req.body.comicName
+    }-from-${start}-to-${end}`;
+
+
+    await launchChrome.processDownloadMultiChapterComic(comicLink, comicName, imageSelector, listChapterSelector, start, end);
+    const downloadZipFile = path.join(rootPath, 'images', `${comicName}.zip`);
+    const archive = archiver('zip');
+    const output = fs.createWriteStream(downloadZipFile)
+
+    output.on('close', function () {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+        res.sendFile(downloadZipFile, downloadOptions(comicName))
+    });
+
+    archive.on('error', function (err) {
+        console.log(err)
+        throw err;
+    });
+
+    archive.pipe(output);
+    archive.directory(path.join(rootPath, 'images', comicName), false);
+    archive.finalize();
 });
 
-app.listen(process.env.PORT || port, () =>
-  console.log(`Application listen on port: ${port}`)
-);
+app.post('/download-images', async (req, res) => {
+    const link = req.body.link;
+    const imageSelector = req.body.imageSelector;
+    const randomFolderName = faker.datatype.uuid();
+    await launchChrome.processDownloadImageByPattern(link, imageSelector, randomFolderName);
+
+    const downloadZipFile = path.join(rootPath, 'images', `${randomFolderName}.zip`);
+    const archive = archiver('zip');
+    const output = fs.createWriteStream(downloadZipFile)
+
+    output.on('close', function () {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+        res.sendFile(downloadZipFile, downloadOptions(randomFolderName))
+    });
+
+    archive.on('error', function (err) {
+        console.log(err)
+        throw err;
+    });
+
+    archive.pipe(output);
+    archive.directory(path.join(rootPath, 'images', randomFolderName), false);
+    archive.finalize();
+})
+
+app.post('/download-images-multipage', async (req, res) => {
+    const link = req.body.link;
+    const imageSelector = req.body.imageSelector;
+    const randomFolderName = faker.datatype.uuid();
+    const pageParamName = req.body.pageParamName;
+    const pageStart = req.body.pageStart;
+    const pageEnd = req.body.pageEnd;
+    await launchChrome.processDownloadImageByPatternWithMultiPage(link, imageSelector, randomFolderName, pageParamName, pageStart, pageEnd);
+
+    const downloadZipFile = path.join(rootPath, 'images', `${randomFolderName}.zip`);
+    const archive = archiver('zip');
+    const output = fs.createWriteStream(downloadZipFile)
+
+    output.on('close', function () {
+        console.log(archive.pointer() + ' total bytes');
+        console.log('archiver has been finalized and the output file descriptor has closed.');
+        res.sendFile(downloadZipFile, downloadOptions(randomFolderName))
+    });
+
+    archive.on('error', function (err) {
+        console.log(err)
+        throw err;
+    });
+
+    archive.pipe(output);
+    archive.directory(path.join(rootPath, 'images', randomFolderName), false);
+    archive.finalize();
+})
+
+
+app.get('/files-folders', async (req, res) => {
+    try {
+        const files = await fsPromise.readdir(path.join(rootPath, "images"));
+        return res.json(files);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+app.delete('/files-folders', async (req, res) => {
+    try {
+        const files = req.body;
+        const removeOptions = {
+            recursive: true
+        }
+        for (const index in files) {
+            if (fs.existsSync(files[index])) {
+                await fsPromise.rm(files[index], removeOptions)
+            }
+        }
+
+        return res.json({message: 'Deleted'})
+    } catch (error) {
+        console.error(err);
+    }
+})
+
+app.listen(process.env.PORT || port, () => console.log(`Application listen on port: ${port}`));
