1890b3512ebc6bce0eaa41d2d2941181b2d11de4
tamnguyen <tamnguyen02121993@gmail.com>
2021-06-14 17:53:54 +0700

    Update config chrome driver

--- launchChrome.js
+++ launchChrome.js
@@ -14,6 +14,7 @@ module.exports = {
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
+        options.addArguments('--disable-dev-shm-usage');
 
 
         const driver = new Builder().forBrowser("chrome")
